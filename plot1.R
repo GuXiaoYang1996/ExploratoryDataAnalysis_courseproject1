27d214df7c59234dc88bbd830f05b99178f39d15
GuXiaoYang1996 <e0326200@u.nus.edu>
2026-04-26 23:40:50 +0800

    initial commit

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_co
 download.file(url,"exdata_data_household_power_consumption.zip",method="curl")
 unzip("exdata_data_household_power_consumption.zip")
 
-# Read data file and convert Date/Time
+# Read data file
 data200702 <- fread("household_power_consumption.txt",skip=66637,nrows=2880,col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 # Plot 1
